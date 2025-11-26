5d3fc674de9fec1e3dc5557ae8d17d69b549d74b
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-11-26 15:53:12 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -5,6 +5,7 @@ export const auth = async (req, res , next) => {
         const {userId ,has} = await req.auth();
         const hasPremiumPlan = await has({plan: 'premium'});
 
+        
         const user = await clerkClient.users.getUser(userId);
 
         if(!hasPremiumPlan && user.privateMetadata.free_usage){
