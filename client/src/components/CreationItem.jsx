ddc717f24113fe3a1cee7eb05ec62f7627fdc5f4
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-12-10 23:17:31 +0530

    Update CreationItem.jsx

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -4,6 +4,7 @@ import Markdown from 'react-markdown'
 const CreationItem = ({item}) => {
     const [expanded ,setExpanded] = useState(false)
 
+    
   return (
     <div onClick={()=> setExpanded(!expanded)} className='p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer'>
         <div className='flex justify-between items-center gap-4'>
