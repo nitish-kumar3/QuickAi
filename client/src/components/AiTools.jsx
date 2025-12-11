420c99a542e932813b36d19ede860450c2ac88ba
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2025-12-11 20:25:23 +0530

    Update AiTools.jsx

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -6,6 +6,7 @@ import { useUser } from '@clerk/clerk-react'
 const AiTools = () => {
    const Navigate = useNavigate()
    const {user} = useUser()
+   
   return (
     <div className='px-4 sm:px-20 xl:px-32 my-24'>
         <div className='text-center'>
